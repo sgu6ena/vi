220026312740bc152ba5af6c8e64c7511ea9c361
Иссар Наталья Сергеевна <issar@callcenter.idknet.com>
2023-11-28 12:05:49 +0200

    autorize from mobile

---
+++ app/(pages)/myidc/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+import React, {useEffect} from 'react';
+import {useRouter, useSearchParams} from "next/navigation";
+import Cookies from "js-cookie";
+import {LINKS} from "@/app/config/links";
+
+const Page = () => {
+    const params = useSearchParams()
+    const {push} = useRouter()
+    const token = params.get('token')
+
+    if (token) {
+        Cookies.set('at', token)
+    }
+
+    useEffect(() => {
+        push(LINKS.START)
+    }, [token]);
+
+    return (
+        <div className={'text-white text-center my-20'}>
+
+        </div>
+    );
+};
+
+export default Page;
